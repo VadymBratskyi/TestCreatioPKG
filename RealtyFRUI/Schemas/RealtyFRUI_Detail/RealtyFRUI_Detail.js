6f15c7c998f973bdc2b59aff319fe23ba42f0d03
v.bratskyi <v.bratskyi@creatio.com>
2026-02-26 13:01:25 +0200

    create application

---
+++ RealtyFRUI/Schemas/RealtyFRUI_Detail/RealtyFRUI_Detail.js
@@ -0,0 +1,8 @@
+define('RealtyFRUI_Detail', [], function () {
+	return {
+		entitySchemaName: 'RealtyFRUI',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ RealtyFRUI/Schemas/RealtyFRUI_MobileFormPage/RealtyFRUI_MobileFormPage.js
@@ -0,0 +1,78 @@
+{
+	"viewConfigDiff": [
+		{
+			"operation": "insert",
+			"name": "Name",
+			"values": {
+				"type": "crt.Input",
+				"label": "$Resources.Strings.Name",
+				"control": "$Name",
+				"placeholder": "",
+				"readonly": false,
+				"multiline": false,
+				"labelPosition": "above"
+			},
+			"parentName": "AreaProfileContainer",
+			"propertyName": "items",
+			"index": 0
+		},
+		{
+			"operation": "merge",
+			"name": "Feed",
+			"values": {
+				"dataSourceName": "PDS",
+				"entitySchemaName": "RealtyFRUI"
+			}
+		}
+	],
+	"viewModelConfigDiff": [
+		{
+			"operation": "merge",
+			"path": [
+				"attributes"
+			],
+			"values": {
+				"Name": {
+					"modelConfig": {
+						"path": "PDS.Name"
+					}
+				},
+				"Id": {
+					"modelConfig": {
+						"path": "PDS.Id"
+					}
+				}
+			}
+		}
+	],
+	"modelConfigDiff": [
+		{
+			"operation": "merge",
+			"path": [],
+			"values": {
+				"dataSources": {
+					"PDS": {
+						"type": "crt.EntityDataSource",
+						"scope": "page",
+						"config": {
+							"entitySchemaName": "RealtyFRUI",
+							"loadParameters": {
+								"options": {
+									"pagingConfig": {
+										"rowCount": 1,
+										"rowsOffset": -1
+									},
+									"sortingConfig": {
+										"columns": []
+									}
+								}
+							},
+							"allowCopyingRecords": false
+						}
+					}
+				},
+				"primaryDataSourceName": "PDS"
+			}
+		}
+	]
+}
