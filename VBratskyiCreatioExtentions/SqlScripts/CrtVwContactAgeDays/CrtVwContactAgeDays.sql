dacb0a16ba87f71125607a59e149b73e977561fb
v.bratskyi <v.bratskyi@creatio.com>
2026-02-26 13:01:05 +0200

    Entity and View, added sql script for view generation

--- VBratskyiCreatioExtentions/SqlScripts/CrtVwContactAgeDays/CrtVwContactAgeDays.sql
+++ VBratskyiCreatioExtentions/SqlScripts/CrtVwContactAgeDays/CrtVwContactAgeDays.sql
@@ -1,5 +1,5 @@
 create or alter view CrtVwContactAgeDays
 as
 select
-Id, Name, BirthDate, DATEDIFF(DAY, BirthDate, GETDATE()) as Age
+Id, Name, BirthDate, DATEDIFF(DAY, BirthDate, GETDATE()) as AgeDays
 from Contact
